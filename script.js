a80db5857170f4d60fdade1d73f1b67c8e69b264
yusuf <yusufay1157@gmail.com>
2022-10-09 11:56:23 +0300

    js sign in

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let buttonInp = document.querySelector("#myInput");
+let result = document.querySelector('#result')
+let resultEmail = document.querySelector('#result-email')
+
+result.style.display = "none"
+resultEmail.style.display = "none";
+
+buttonInp.onclick = function () {
+    if(!email.value){
+        resultEmail.innerHTML = "Hata";
+        resultEmail.style.display = "block";
+        resultEmail.style.color = 'red';
+    }
+    if(password.value < 8) {
+        result.innerHTML = 'Hata';
+        result.style.display = 'block'
+        result.style.color = 'red'
+    }
+}
